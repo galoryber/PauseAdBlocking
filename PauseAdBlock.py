8e679b0a97a4a40f63f05fae9bafd6de7659d225
galoryber <51980089+galoryber@users.noreply.github.com>
2024-03-10 12:30:55 -0500

    Update PauseAdBlock.py
    Not exactly what I wanted, but working enough that it isn't worth the extra effort.

--- PauseAdBlock.py
+++ PauseAdBlock.py
@@ -1,17 +1,17 @@
-import requests, time, logging
+import requests, time, logging, threading
 import RPi.GPIO as gpio
 
-logging.basicConfig(filename='/root/scripts/piButton.log', format='%(levelname)s:%(asctime)s:%(message)s', encoding>
+logging.basicConfig(filename='/root/scripts/piButton.log', format='%(levelname)s:%(asctime)s:%(message)s', encoding='utf-8', level=logging.DEBUG)
 
 # On button press, pause PiHole DNS blocking for X number of minutes
 
 piHoleIP = "192.168.100.151"
+#pinChannel = 26 # GPIO26 == PIN 37
 ledPinChannel = 31 #GPIO6 == PIN 31 https://i.pinimg.com/736x/c6/a9/39/c6a939f2c365e141abb18947752dbe8b.jpg
 pinBoard= 37 # setmode(gpio.BOARD) Board 37 -> GPIO26
 pauseTime = 120 # Two minutes
 ledTimer = 0
 
-# Authenticate to PiHole
 def getAuth():
     with open("/etc/pihole/setupVars.conf","r") as f:
         auth = []
@@ -21,38 +21,46 @@ def getAuth():
     authString = auth[0].split('=')[1].rstrip() # WEBPASSWORD=asdfValueasdf
 
     return authString
+
 webPass = getAuth()
 
-# Self explanatory function name
 def pause_AdBlocking(whatarg):
     logging.info("Pin " + str(whatarg) + " was pressed")
     piholeURL = "http://{}/admin/api.php?disable={}&auth={}".format(piHoleIP, pauseTime, webPass)
-    response = requests.get(piholeURL)
-    logging.info("Ad Blocking was paused for the next two minutes\n")
-    ledWatcher(pauseTime)
+    resp = requests.get(piholeURL)
+    logging.info("Ad Blocking was paused for the next two minutes")
+    # # turn on notification LED
+    t = threading.Thread(target=ledWatcher, args=[pauseTime])
+    t.run()
+    return resp
 
-# Build separate function for LED controls
 def ledWatcher(onTimer):
     logging.info("Turning on " + str(ledPinChannel) + " for " + str(onTimer))
-    gpio.output(ledPinChannel, True)
-    logging.info("LED should be on")
-    time.sleep(onTimer)
+    while onTimer > 0:
+        gpio.output(ledPinChannel, True)
+        sleepTime = 1
+        onTimer -= sleepTime
+        time.sleep(sleepTime)
+    logging.info("Turning off " + str(ledPinChannel) + "\n\n")
     gpio.output(ledPinChannel, False)
-    logging.info("LED should be off now")
 
-# Function have been defined
-# setup pins and start operating
+
 gpio.setmode(gpio.BOARD)
+
 # setup button pin
 gpio.setup(pinBoard, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
 # setup led pin
 gpio.setup(ledPinChannel, gpio.OUT)
+
 # listen for button press events
+# Appears that subsequent button presses are 'queued' for after the function returns
 gpio.add_event_detect(pinBoard, gpio.RISING, callback=pause_AdBlocking) # Setup event on pin rising edge
+
 
 try:
     while True:
-        time.sleep(0.25)
+        time.sleep(0.25) # Keeps program running forever, listening for button events, sleep keeps CPU usage low
         pass
 
 except KeyboardInterrupt:
